a3bb06b7f666e3e9949b97503be1493599dc1490
katkonovalova <katkonovalova>
2020-10-01 15:21:36 +0300

    refactoring of methods click and type

--- src/main/java/com/telRan/tests/LoginTrelloTests.java
+++ src/main/java/com/telRan/tests/LoginTrelloTests.java
@@ -3,6 +3,7 @@ package com.telRan.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -19,33 +20,46 @@ public class LoginTrelloTests {
         wd.manage().window().maximize();
         //open site
         wd.navigate().to("https://trello.com/");
-
-
     }
     @Test
     public void testLoginPositiveTest() throws InterruptedException {
-        //click on login button on welcome page
-        wd.findElement(By.cssSelector("[href='/login']")).click();
-        //fillLoginForm
-        //fillEmail
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("katkonovalova88@gmail.com");
-        //wait
-        Thread.sleep(10000);
-        //fillPassword
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys("Aa123123_");
-        //clickSubmitButton
-        wd.findElement(By.id("login")).click();
-        //userLoggedIn
 
-
+        login("katerynakonovalov@gmail.com", "A123123a_");
+        Assert.assertTrue(isAvatarPresent());
     }
+
+    public void login(String email, String password) throws InterruptedException {
+        click(By.cssSelector("[href='/login']"));
+        type(By.name("user"), email);
+
+        click(By.id("login"));
+
+        Thread.sleep(10000);
+        type(By.id("password"), password);
+        click(By.id("login-submit"));
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public void click(By locator){
+        wd.findElement(locator).click();
+    }
+    public boolean isAvatarPresent(){
+       // return wd.findElements(By.xpath("//button[@data-test-id='header-member-menu-button']")).size()>0;
+        return isElementPresent(By.xpath("//button[@data-test-id='header-member-menu-button']"));
+    }
+    public boolean isElementPresent(By locator){
+        return wd.findElements(locator).size()>0;
+    }
+
     @AfterClass
     public void tearDown(){
 
     }
+
 
 }
